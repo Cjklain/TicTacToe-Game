0fcc70c84f6da179ec205b08dd3bd37d4b95f280
Karol M <KarolMMM01@gmail.com>
2022-03-30 13:53:12 +0200

    added gui working version

--- index.js
+++ index.js
@@ -1,18 +1,47 @@
-const gameBoard = (() => {
-    board = [];
-    
-    for(let i=0; i<9; i++){
+const startButton = document.querySelector('.start')
+startButton.addEventListener('click',makeBoard)
+const board = document.querySelector('.board')
+makeBoard();
+
+function makeBoard(){
+    const board = document.querySelector('.board')
+
+    if(board.children.length>1)return
+
+    for(let i=0; i <9; i++){
         const box = document.createElement('div');
-        box.classList.add(`box`)
-        box.classList.add(`number-${i}`)
-        board.push(box)
+        box.classList.add('box')
+        box.dataset.number = `${i+1}`
+        board.appendChild(box)
     }
-   
-    const boardSpace = document.querySelector('.board')
-    board.forEach(item => boardSpace.appendChild(item));
+    markMove()
+}
 
-    console.log(boardSpace);
+function markMove(){
+    let whoseTurn = [];
+    const boxs = document.querySelectorAll('.box');
+    boxs.forEach(box => box.addEventListener('click',(e) => {
+        const clickedBox = e.currentTarget;
+        if(whoseTurn[whoseTurn.length-1] === 0){
+            clickedBox.classList.add('circle')
+            whoseTurn.push(1)
+            checkResult(clickedBox)
+        } else {
+            clickedBox.classList.add('cross') 
+            whoseTurn.push(0)
+            checkResult(clickedBox)
+        }
+        // check if win
+        console.log('clicked');
+    },{once: true}))
+}
 
-    const boxs = document.querySelectorAll('.box')
-    console.log(boxs);
-})();
+const dataToCheck = [];
+
+function checkResult(e){
+    let position = e.dataset.number; 
+    let currentClass = e.classList.contains('cross');
+    let singleBox = [position,currentClass]
+    dataToCheck.push(singleBox);
+}
+
